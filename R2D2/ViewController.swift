5a5cb6a8db64d7685074fe420732283498cae9cb
aritropaul <aritro.paul61@gmail.com>
2019-12-15 00:56:32 +0530

    Initial Commit

---
+++ R2D2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  R2D2
+//
+//  Created by Aritro Paul on 15/12/19.
+//  Copyright © 2019 Aritro Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
